da335a826a63b9e4f850dbefa500b687ef6c032d
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-04-20 17:47:31 -0400

    added initial structure of app

---
+++ quiz_app/game.js
@@ -0,0 +1,78 @@
+const question = document.getElementById("question") //DOM query to reference to the question 
+const choices = Array.from(document.getElementsByClassName( 'choice-text')); //Convert the html collection to an array
+
+let currentQuestion = {};
+let acceptingAnswers = false; //This is to prevent answering before the page is loaded and ready
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
+
+let questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript??',
+        choice1: '<script>',
+        choice2: '<javascript>',
+        choice3: '<js>',
+        choice4: '<scripting>',
+        answer: 1,
+    },
+    {
+        question:
+            "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script name='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<script file='xxx.js'>",
+        answer: 3,
+    },
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    },
+];
+
+//Constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions] //Used the spread operator to copy in all of the questions from the array
+    getNewQuestion();
+};
+
+getNewQuestion = () => { //Arrow syntax for more concise functions
+        if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+            //Go to end page
+            return window.location.assign("/end.html");
+        }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestion.length); //Math.random gives a random number between 0 and 1. Multiplying by 3 gives a number between 0 and 3. Math.floor rounds down to integer*/
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number']; //Gets the number property from the data-set property 
+        choice.innerText = currentQuestion['choice' + number]; //Out of the current question we get the choice out of it
+    });
+    availableQuestions.splice(questionIndex, 1); //Take the available question array and get rid of the question we just used.
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame(); //Need to call the startGame function
